207bdd2f3d75e03522f125394cc2bd39f797c47f
Rasanga Lakshith <rasanga@rooster.org>
2023-03-24 20:15:44 +0530

    feature: delete user profile added

--- screens/Mentor/SubScreens/MentorProfileSubPage.js
+++ screens/Mentor/SubScreens/MentorProfileSubPage.js
@@ -10,8 +10,19 @@ import {
 } from 'react-native';
 import React, { useEffect } from 'react';
 import TopBar from '../../../components/Common/TopBar';
-import { doc, getDoc } from 'firebase/firestore';
+import {
+  doc,
+  getDoc,
+  deleteDoc,
+  collection,
+  getDocs,
+  query,
+  where,
+} from 'firebase/firestore';
 import { db } from '../../../firebase';
+import { Alert } from 'react-native';
+import { deleteUser, getAuth } from 'firebase/auth';
+
 import { useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation } from '@react-navigation/native';
@@ -49,6 +60,49 @@ const MentorProfileSubPage = () => {
     fetchData();
   }, [data]);
 
+  const handleDeleteAccount = async () => {
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    if (user) {
+      // Show confirmation alert
+      Alert.alert(
+        'Delete Account',
+        'Are you sure you want to delete your account?',
+        [
+          {
+            text: 'Cancel',
+            style: 'cancel',
+          },
+          {
+            text: 'Yes',
+            onPress: async () => {
+              try {
+                const usersRef = collection(db, 'Users');
+
+                const q = query(usersRef, where('email', '==', user.email));
+                const querySnapshot = await getDocs(q);
+                let userDocId = null;
+
+                await querySnapshot.forEach((doc) => {
+                  userDocId = doc.id;
+                });
+
+                await deleteDoc(doc(db, 'Users', userDocId));
+                await deleteUser(user);
+
+                navigation.navigate('login');
+              } catch (error) {
+                console.error('Error deleting account:', error);
+              }
+            },
+          },
+        ],
+        { cancelable: false }
+      );
+    }
+  };
+
   return (
     <>
       {loading ? (
@@ -58,129 +112,155 @@ const MentorProfileSubPage = () => {
           style={{ marginVertical: '100%' }}
         />
       ) : (
-        <View style={styles.container}>
-          {/* Top bar */}
-          <TopBar title={'Profile'} />
+        data && (
+          <View style={styles.container}>
+            {/* Top bar */}
+            <TopBar title={'Profile'} />
 
-          {/* Content */}
-          <View
-            style={{
-              marginHorizontal: 10,
-              marginVertical: 30,
-            }}
-          >
-            {/* Header Part */}
-            <View style={{ flexDirection: 'row', padding: 10 }}>
+            {/* Content */}
+            <View
+              style={{
+                marginHorizontal: 10,
+                marginVertical: 30,
+              }}
+            >
+              {/* Header Part */}
+              <View style={{ flexDirection: 'row', padding: 10 }}>
+                <View
+                  style={{
+                    backgroundColor: '#D9D9D9',
+                    borderRadius: 100,
+                    padding: 2,
+                    marginRight: 10,
+                  }}
+                >
+                  <Image
+                    source={{
+                      uri:
+                        data.image !== null
+                          ? data.image
+                          : 'https://cdn-icons-png.flaticon.com/512/2354/2354573.png',
+                    }}
+                    style={styles.userImage}
+                  />
+                </View>
+
+                <View style={{ flexDirection: 'column', alignSelf: 'center' }}>
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      marginLeft: 8,
+                      marginTop: -4,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        color: '#1A2042',
+                        fontWeight: '500',
+                        fontSize: 24,
+                      }}
+                    >
+                      {data.name}
+                    </Text>
+                  </View>
+                  <View style={{ marginLeft: 8 }}>
+                    <Text
+                      style={{
+                        color: '#1A2042',
+                        fontSize: 16,
+                        fontWeight: '400',
+                      }}
+                    >
+                      {data.position}
+                    </Text>
+                  </View>
+                  <View style={{ marginLeft: 8 }}>
+                    <Text
+                      style={{
+                        color: '#1A2042',
+                        fontSize: 16,
+                        fontWeight: '500',
+                      }}
+                    >
+                      {data.sessions} sessions
+                    </Text>
+                  </View>
+                </View>
+              </View>
+
+              {/* Field data */}
               <View
                 style={{
-                  backgroundColor: '#D9D9D9',
-                  borderRadius: 100,
-                  padding: 2,
-                  marginRight: 10,
+                  maxHeight: 450,
+                  paddingTop: 10,
+                  paddingLeft: 20,
+                  paddingRight: 20,
                 }}
               >
-                <Image
-                  source={{
-                    uri: data.image,
-                  }}
-                  style={styles.userImage}
-                />
+                <ScrollView>
+                  <View>
+                    <Text style={styles.mainFieldName}>Bio</Text>
+                    <Text style={styles.fieldData}>{data.bio}</Text>
+                    <Text style={styles.mainFieldName}>Email</Text>
+                    <Text style={styles.fieldData}>{data.email}</Text>
+                    <Text style={styles.mainFieldName}>Date of birth</Text>
+                    <Text style={styles.fieldData}>{data.age}</Text>
+                    <Text style={styles.mainFieldName}>Working time</Text>
+                    <Text style={styles.fieldData}>
+                      {formatTime(data.workingTimeFrom)} -{' '}
+                      {formatTime(data.workingTimeTo)}
+                    </Text>
+                  </View>
+                </ScrollView>
               </View>
 
-              <View style={{ flexDirection: 'column', alignSelf: 'center' }}>
-                <View
-                  style={{ flexDirection: 'row', marginLeft: 8, marginTop: -4 }}
-                >
-                  <Text
-                    style={{
-                      color: '#1A2042',
-                      fontWeight: '500',
-                      fontSize: 24,
-                    }}
-                  >
-                    {data.name}
-                  </Text>
-                </View>
-                <View style={{ marginLeft: 8 }}>
-                  <Text
-                    style={{
-                      color: '#1A2042',
-                      fontSize: 16,
-                      fontWeight: '400',
-                    }}
-                  >
-                    {data.position}
-                  </Text>
-                </View>
-                <View style={{ marginLeft: 8 }}>
-                  <Text
-                    style={{
-                      color: '#1A2042',
-                      fontSize: 16,
-                      fontWeight: '500',
-                    }}
-                  >
-                    {data.sessions} sessions
-                  </Text>
-                </View>
-              </View>
-            </View>
-
-            {/* Field data */}
-            <View
-              style={{
-                maxHeight: 450,
-                paddingTop: 10,
-                paddingLeft: 20,
-                paddingRight: 20,
-              }}
-            >
-              <ScrollView>
-                <View>
-                  <Text style={styles.mainFieldName}>Bio</Text>
-                  <Text style={styles.fieldData}>{data.bio}</Text>
-                  <Text style={styles.mainFieldName}>Email</Text>
-                  <Text style={styles.fieldData}>{data.email}</Text>
-                  <Text style={styles.mainFieldName}>Date of birth</Text>
-                  <Text style={styles.fieldData}>{data.age}</Text>
-                  <Text style={styles.mainFieldName}>Working time</Text>
-                  <Text style={styles.fieldData}>
-                    {formatTime(data.workingTimeFrom)} -{' '}
-                    {formatTime(data.workingTimeTo)}
-                  </Text>
-                </View>
-              </ScrollView>
-            </View>
-
-            {/* Buttons */}
-            <View
-              style={{
-                position: 'absolute',
-                top: windowHeight - 180,
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                // marginTop: 20,
-              }}
-            >
-              <TouchableOpacity
+              {/* Buttons */}
+              <View
                 style={{
-                  backgroundColor: '#3D3EEF',
-                  width: '100%',
-                  height: 50,
-                  borderRadius: 10,
-                  justifyContent: 'center',
+                  position: 'absolute',
+                  top: windowHeight - 180,
+                  flexDirection: 'row',
                   alignItems: 'center',
-                  alignSelf: 'center',
-                  marginTop: 30,
-                  //   marginHorizontal: 0,
+                  gap: 12,
+                  // marginTop: 20,
                 }}
-                onPress={() => navigation.navigate('mentorProfileUpdate')}
               >
-                <Text style={styles.buttonText}>Edit</Text>
-              </TouchableOpacity>
+                <TouchableOpacity
+                  style={{
+                    backgroundColor: '#EF3D72',
+                    width: 180,
+                    height: 50,
+                    borderRadius: 10,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    alignSelf: 'center',
+                    marginTop: 30,
+                  }}
+                  onPress={handleDeleteAccount}
+                >
+                  <Text style={styles.buttonText}>Delete Account</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={{
+                    backgroundColor: '#3D3EEF',
+                    width: 180,
+                    height: 50,
+                    borderRadius: 10,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    alignSelf: 'center',
+                    marginTop: 30,
+                    marginLeft: 10,
+                    //   marginHorizontal: 0,
+                  }}
+                  onPress={() => navigation.navigate('mentorProfileUpdate')}
+                >
+                  <Text style={styles.buttonText}>Edit</Text>
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
-        </View>
+        )
       )}
     </>
   );
